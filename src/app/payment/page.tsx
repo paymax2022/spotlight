214f87e7e6bff8d76c4681081fc04e1edd0aae37
Adekunle Victor <adekunlevictor07@gmail.com>
2025-03-12 20:22:03 +0100

    100%done

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,27 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-
+import { usePathname } from "next/navigation";
 
 const Page = () => {
-  const searchParams = useSearchParams();
+  const [paramAmount, setParamAmount] = React.useState<string | null>(null);
+  const [paramPoint, setParamPoint] = React.useState<string | null>(null);
+
+  // Extract query parameters manually from the URL
+  useEffect(() => {
+    
+    const queryString = window.location.search; // Get the query string from the URL
+    const urlParams = new URLSearchParams(window.location.search); // Parse the query string
+    console.log(urlParams)
+   
+    const amount = urlParams.get("amount"); // Get the "amount" parameter
+    const point = urlParams.get("point"); // Get the "point" parameter
+
+    setParamAmount(amount); 
+    setParamPoint(point); 
+  }, []);
   
-  // if (searchParams) {
-  //   console.log(searchParams.get("amount"));
-  //   console.log(searchParams.get("point"));
-  // } else {
-  //   console.log("Params is null");
-  // }
+ 
+
   return (
     <div className="px-4">
-     
       <div>
         <p className="text-[36px] font-bold">VOTING</p>
         <p className="text-[24px] italic text-gray-300">
-          YOU ARE MAKING A <span className="text-[#6200DF]">{searchParams ? searchParams.get("amount") : "N/A"}</span> VOTING
-          FOR  <span className="text-[#6200DF]">{searchParams ? searchParams.get("point") : "N/A"}</span> POINTS
+          YOU ARE MAKING A{" "}
+          <span className="text-[#6200DF]">{paramAmount || "N/A"}</span> VOTING
+          FOR{" "}
+          <span className="text-[#6200DF]">{paramPoint || "N/A"}</span> POINTS
         </p>
       </div>
 
@@ -31,6 +42,7 @@ const Page = () => {
           Please fill in the form below. Mandatory fields are marked with a *
         </p>
       </div>
+
       <div className="space-y-2 mt-10">
         <input
           type="text"
@@ -53,6 +65,7 @@ const Page = () => {
           className="w-full p-3 bg-black border border-purple-500 text-gray-300 rounded-md focus:outline-none"
         />
       </div>
+
       <div className="my-8">
         <button className="bg-white text-black font-semibold px-6 py-3 rounded-full w-full sm:w-auto shadow-md hover:bg-gray-200">
           PAY
