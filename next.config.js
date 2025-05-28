5fbd2b02a5ea02f486c2618f0ebd9eef51fb3927
Muhammad Umair Ali <m.umair.ali200@gmail.com>
2025-05-28 11:48:06 +0500

    Replace old website with Next.js version

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [], // Add any image domains you need here
+  },
+  // Add any other Next.js config options here
+}
+
+module.exports = nextConfig 
--- updated-website/main_file/.next/static/chunks/app/privacy-policy/page-a30e8c54897319ff.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2902,8282],{1920:(e,s,n)=>{Promise.resolve().then(n.bind(n,2978)),Promise.resolve().then(n.bind(n,946))}},e=>{var s=s=>e(e.s=s);e.O(0,[7244,4752,6095,8441,1684,7358],()=>s(1920)),_N_E=e.O()}]);
