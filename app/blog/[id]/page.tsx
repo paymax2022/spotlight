5086708dd7a4ffa2b9f69612cd3b6fc3acf5384c
Muhammad Umair Ali <m.umair.ali200@gmail.com>
2025-07-02 19:48:16 +0500

    Refactor blog/[id] to server component and move client logic to BlogDetailsClient for static export compatibility

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,42 +1,15 @@
-'use client'
-import React from "react"
 import Layout from "@/components/layout/Layout"
 import data from "@/util/blog.json"
-import { useParams } from "next/navigation"
-import { useEffect, useState } from "react"
+import BlogDetailsClient from "./BlogDetailsClient"
 
-interface Post {
-    id: number
-    title: string
-    img: string
-    category: string
-    author: string
-    date: string
-}
-
-export default function BlogDetails() {
-    let Router = useParams()
-    const [blogPost, setBlogPost] = useState<Post | null>(null)
-    const id = Router?.id
-
-    useEffect(() => {
-        if (id) {
-            const post = data?.find((post: Post) => String(post.id) === String(id))
-            setBlogPost(post || null)
-        }
-    }, [id])
-
+export default function BlogDetailsPage({ params }: { params: { id: string } }) {
+    const id = params.id;
+    const blogPost = data.find((post: { id: number }) => String(post.id) === String(id)) || null;
     return (
-        <>
-            <Layout>
-                {blogPost && (
-                    <>
-                        {blogPost.title}
-                    </>
-                )}
-            </Layout>
-        </>
-    )
+        <Layout>
+            <BlogDetailsClient blogPost={blogPost} />
+        </Layout>
+    );
 }
 
 export async function generateStaticParams() {
